a292ad0a393d0ab533d62443d99a627406581361
Arseniy <queezz@gmail.com>
2020-03-05 17:50:17 +0900

    initial commit

---
+++ resource.h
@@ -0,0 +1,56 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ASDEX_control.RC
+//
+#define IDS_STRING1                     1
+#define IDS_STRING2                     2
+#define IDS_STRING3                     3
+#define IDS_STRING4                     4
+#define IDS_STRING5                     5
+#define IDS_STRING6                     6
+#define IDD_DIALOG1                     101
+#define IDI_ICON1                       102
+#define IDB_GREEN                       106
+#define IDB_RED                         107
+#define IDE_MYADRS                      1000
+#define IDE_GPADRS                      1001
+#define IDC_TXDATA                      1002
+#define IDE_TXDATA                      1003
+#define IDE_RXDATA                      1004
+#define IDB_INIT                        1006
+#define IDB_PRO_INS                     1007
+#define IDB_PRO_RUN                     1008
+#define IDB_TXDATA                      1009
+#define IDT_STATUS                      1010
+#define IDB_PRO_STOP                    1015
+#define IDC_AGSTATUS                    1016
+#define IDC_EDIT_FM                     1017
+#define IDC_EDIT_FI                     1018
+#define IDC_EDIT_EC                     1019
+#define IDC_EDIT_CF                     1020
+#define IDC_EDIT_FMV                    1021
+#define IDC_EDIT_FIV                    1022
+#define IDC_EDIT_ECV                    1023
+#define IDC_EDIT_CFV                    1024
+#define IDC_EDIT_GI                     1025
+#define IDC_EDIT_GIV                    1026
+#define IDC_EDIT_SI                     1027
+#define IDC_EDIT_SIV                    1028
+#define IDC_AG1                         1029
+#define IDC_AG2                         1030
+#define IDC_AG3                         1031
+#define IDC_START_AG                    1032
+#define IDC_STOP                        1033
+#define IDC_SLIDER1                     1034
+#define IDC_LED                         1035
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1037
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
